f46d2f7acaffe2d439493445903b5b5164bfa1d4
Devesh Sawale <im007dev@gmail.com>
2022-06-28 22:11:35 +0530

    Created models

--- OJ/models.py
+++ OJ/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Problems(models.Model):
+    statement = models.CharField(max_length=1200)
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=1200)
+    difficulty = models.CharField(max_length=10)
+
+class Solutions(models.Model):
+    problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
+    verdict = models.CharField(max_length=200)
+    submitted_at = models.DateTimeField('date submitted')
+
+class TestCases(models.Model):
+    input = models.CharField(max_length=200)
+    output = models.CharField(max_length=200)
+    problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
