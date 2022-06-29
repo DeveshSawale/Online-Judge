92ae7122e1f42e6138436afbdda645dbc2418e83
Devesh Sawale <im007dev@gmail.com>
2022-06-29 21:59:22 +0530

    made changes in models

--- OJ/models.py
+++ OJ/models.py
@@ -1,6 +1,12 @@
+
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    password = models.CharField(max_length=50)
+    score = models.IntegerField(default = 0)
+    submissions = models.IntegerField(default=0)
 
 class Problems(models.Model):
     statement = models.CharField(max_length=1200)
@@ -10,6 +16,8 @@ class Problems(models.Model):
 
 class Solutions(models.Model):
     problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    language = models.CharField(max_length = 10)
     verdict = models.CharField(max_length=200)
     submitted_at = models.DateTimeField('date submitted')
 
